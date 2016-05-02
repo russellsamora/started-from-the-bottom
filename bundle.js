248754d4e365e56ce0b8ee5d390bb27e70bec0c1
russellgoldenberg <russellgoldenberg@gmail.com>
2016-05-02 12:53:51 -0400

    multiple teams and stretches

--- bundle.js
+++ bundle.js
@@ -1,15 +1,21 @@
 'use strict';
 
 (function () {
-	var STEPS = ['top-and-bottom', 'path-single', 'path-all', 'path-normalized', 'path-duration'];
-	var RADIUS_LARGE = 6;
-	var RADIUS_SMALL = 4;
+	var TEAM_NAME_DICT = { 'ATL': 'Hawks', 'BOS': 'Celtics', 'BRK': 'Nets', 'CHI': 'Bulls', 'CHO': 'Hornets', 'CLE': 'Cavaliers', 'DAL': 'Mavericks', 'DEN': 'Nuggest', 'DET': 'Pistons', 'GSW': 'Warriors', 'HOU': 'Rockets', 'IND': 'Pacers', 'LAC': 'Clippers', 'LAL': 'Lakers', 'MEM': 'Grizzlies', 'MIA': 'Heat', 'MIL': 'Bucks', 'MIN': 'Timberwolves', 'NOP': 'Pelicans', 'NYK': 'Knicks', 'OKC': 'Thunder', 'ORL': 'Magic', 'PHI': '76ers', 'PHO': 'Suns', 'POR': 'Trail Blazers', 'SAC': 'Kings', 'SAS': 'Spurs', 'TOR': 'Raports', 'UTA': 'Jazz', WAS: 'Wizards' };
+	var COUNT_TO_WORD = ['zero', 'one', 'two', 'three', 'four', 'five'];
+	var STEPS = ['top-and-bottom', 'warriors', 'stretch-single', 'stretch-all', 'stretch-normalized', 'stretch-duration'];
 	var SECOND = 1000;
 	var EXIT_DURATION = SECOND;
 	var MARGIN = { top: 20, right: 40, bottom: 40, left: 40 };
-	var outerWidth = 640;
-	var outerHeight = 480;
+	var GRAPHIC_MARGIN = 20;
+	var RATIO = 16 / 9;
+	var SECTION_WIDTH = 320;
 
+	var singleTeam = 'GSW';
+	var outerWidth = 0;
+	var outerHeight = 0;
+	var radiusSmall = 0;
+	var radiusLarge = 0;
 	var previousStep = 0;
 	var dir = 0;
 	var chartWidth = 0;
@@ -17,10 +23,18 @@
 	var data = [];
 	var dataByTeam = [];
 	var svg = null;
+	var stretchesCompleted = 0;
+	var stretchesMedian = 0;
 
 	var INTERPOLATE = 'step';
 	var xScale = d3.time.scale();
 	var yScale = d3.scale.linear();
+	// const createLineAll = d3.svg.line()
+	// 	.defined(d => d.rank)
+	// 	.interpolate(INTERPOLATE)
+	// 	.x(d => xScale(d.seasonFormatted))
+	// 	.y(d => yScale(d.rank))
+
 	var createLine = d3.svg.line().defined(function (d) {
 		return d.rank;
 	}).interpolate(INTERPOLATE).x(function (d) {
@@ -42,16 +56,75 @@
 		});
 	}
 
-	function createDropdown() {
-		var options = dataByTeam.map(function (team) {
-			return '<option>' + team.key + '</option>';
-		}).join();
-		var el = document.querySelector('.teams');
-		el.innerHTML = options;
-		el.addEventListener('change', function (e) {
-			var text = e.target.options[e.target.selectedIndex].text;
-			drawTeam(text);
+	function calculateStretch(team) {
+		var indices = team.values.map(function (v, i) {
+			return v.start || v.stop ? i : -1;
+		}).filter(function (v) {
+			return v > -1;
 		});
+
+		var len = indices.length;
+		var newLen = Math.floor(len / 2) * 2;
+		var sliced = indices.slice(0, newLen);
+		var completed = sliced.map(function (v, i) {
+			var streak = i % 2 ? v - sliced[i - 1] : -1;
+			return streak;
+		}).filter(function (v) {
+			return v > 0;
+		}).reduce(function (previous, current) {
+			return previous.concat(current);
+		}, []);
+
+		return { indices: indices, completed: completed };
+	}
+
+	function addStretches(values) {
+		var active = false;
+		return values.map(function (season) {
+			var bottom = season.bottom;
+			var top = season.top;
+
+			if (bottom && !active) {
+				season.start = true;
+				season.stretch = true;
+				active = true;
+			} else if (top && active) {
+				season.stop = true;
+				season.stretch = true;
+				active = false;
+			}
+			if (active) season.stretch = true;
+			return season;
+		});
+	}
+
+	function getStretches(team) {
+		var indices = team.stretches.indices;
+
+		var len = indices.length;
+		var newLen = Math.floor(len / 2) * 2;
+		var sliced = indices.slice(0, newLen);
+
+		var stretches = sliced.map(function (index, i) {
+			var a = index + 1;
+			var b = sliced[i - 1];
+			return i % 2 ? team.values.slice(b, a) : null;
+		}).filter(function (d) {
+			return d;
+		});
+
+		return stretches;
+	}
+
+	function getAverageDiff(count) {
+		var diff = count - stretchesMedian;
+		if (diff < 2) {
+			return 'shorter than';
+		} else if (diff > 2) {
+			return 'longer than';
+		} else {
+			return 'about';
+		}
 	}
 
 	function getStepData(step) {
@@ -59,33 +132,46 @@
 			case 'top-and-bottom':
 				{
 					return {
-						line: [],
-						dot: data.filter(function (d) {
+						all: [],
+						wins: data.filter(function (d) {
 							return d.wins;
-						})
+						}),
+						stretches: []
 					};
 				}
 
-			case 'path-single':
+			case 'warriors':
 				{
 					var team = dataByTeam.filter(function (d) {
 						return d.key === 'GSW';
 					});
 					return {
-						line: team,
-						dot: team[0].values.filter(function (d) {
+						all: team,
+						wins: team[0].values.filter(function (d) {
 							return d.wins;
-						})
+						}),
+						stretches: []
 					};
 				}
 
-			case 'path-all':
+			case 'stretch-single':
+				{
+					var _team = dataByTeam.filter(function (d) {
+						return d.key === singleTeam;
+					});
+					return {
+						all: _team,
+						wins: _team[0].values.filter(function (d) {
+							return d.wins;
+						}),
+						stretches: getStretches(_team[0])
+					};
+				}
+
+			case 'stretch-normalized':
 				{}
 
-			case 'path-normalized':
-				{}
-
-			case 'path-duration':
+			case 'stretch-duration':
 				{}
 
 			default:
@@ -102,7 +188,7 @@
 	}
 
 	function transitionPath(path) {
-		path.transition().duration(SECOND * 4).attrTween('stroke-dasharray', tweenDash);
+		path.transition().ease('quad-in-out').duration(SECOND * 3).attrTween('stroke-dasharray', tweenDash);
 	}
 
 	function stepGraphic(step) {
@@ -110,50 +196,54 @@
 		previousStep = step;
 
 		var chartGroup = svg.select('.chart');
-		var lineGroup = chartGroup.select('.line-group');
-		var dotGroup = chartGroup.select('.dot-group');
+		var allGroup = chartGroup.select('.all-group');
+		var winsGroup = chartGroup.select('.wins-group');
+		var stretchGroup = chartGroup.select('.stretch-group');
 
 		// DATA
 		var stepData = getStepData(STEPS[step]);
-		var lineSelection = lineGroup.selectAll('.line').data(stepData.line);
-		var dotSelection = dotGroup.selectAll('.dot').data(stepData.dot, function (d) {
+		var allSelection = allGroup.selectAll('.all').data(stepData.all);
+		var winsSelection = winsGroup.selectAll('.wins').data(stepData.wins, function (d) {
 			return d.id;
 		});
+		var stretchSelection = stretchGroup.selectAll('.stretch').data(stepData.stretches);
+
+		console.log(stepData);
 
 		// UPDATE
 		switch (STEPS[step]) {
 			case 'top-and-bottom':
 				{
-					dotSelection.enter().append('circle').attr('class', function (d) {
-						return 'dot ' + (d.bottom ? 'bottom' : '') + ' ' + (d.top ? 'top' : '');
+					winsSelection.enter().append('circle').attr('class', function (d) {
+						return 'wins ' + (d.bottom ? 'bottom' : '') + ' ' + (d.top ? 'top' : '');
 					}).attr('r', 0).attr('cx', function (d) {
 						return xScale(d.seasonFormatted);
 					}).attr('cy', function (d) {
 						return yScale(d.rank);
 					});
 
-					dotSelection.transition().duration(SECOND * 2).delay(function (d) {
-						return d.rank * 75;
-					}).ease('quad-in-out').attr('r', RADIUS_SMALL);
+					winsSelection.transition().duration(SECOND * 2).delay(function (d) {
+						return d.rank * 75 + (dir === 0 ? 0 : EXIT_DURATION);
+					}).ease('quad-in-out').attr('r', radiusSmall);
 					break;
 				}
 
-			case 'path-single':
+			case 'warriors':
 				{
-					lineSelection.enter().append('path').attr('class', 'line');
+					allSelection.enter().append('path').attr('class', 'all');
 
-					lineSelection.attr('d', function (d) {
+					allSelection.attr('d', function (d) {
 						return createLine(d.values);
 					}).call(transitionPath);
 
-					dotSelection.enter().append('circle').attr('class', function (d) {
-						return 'dot ' + (d.bottom ? 'bottom' : '') + ' ' + (d.top ? 'top' : '');
+					winsSelection.enter().append('circle').attr('class', function (d) {
+						return 'wins ' + (d.bottom ? 'bottom' : '') + ' ' + (d.top ? 'top' : '');
 					}).attr('r', 0).attr('cy', function (d) {
 						return yScale(d.rank);
 					});
 
-					dotSelection.transition().delay(EXIT_DURATION).duration(SECOND * 2).ease('elastic').attr('r', function (d) {
-						return d.bottom || d.top ? RADIUS_LARGE : RADIUS_SMALL;
+					winsSelection.transition().delay(EXIT_DURATION).duration(SECOND * 2).ease('elastic').attr('r', function (d) {
+						return d.bottom || d.top ? radiusLarge : radiusSmall;
 					}).attr('cx', function (d) {
 						return xScale(d.seasonFormatted);
 					}).attr('cy', function (d) {
@@ -162,17 +252,53 @@
 					break;
 				}
 
-			case 'path-all':
+			case 'stretch-single':
+				{
+					allSelection.enter().append('path').attr('class', 'all');
+
+					allSelection.attr('d', function (d) {
+						return createLine(d.values);
+					});
+
+					stretchSelection.enter().append('path').attr('class', 'stretch');
+
+					stretchSelection.attr('d', function (d) {
+						return createLine(d);
+					}).call(transitionPath);
+
+					winsSelection.enter().append('circle').attr('class', function (d) {
+						return 'wins ' + (d.bottom ? 'bottom' : '') + ' ' + (d.top ? 'top' : '');
+					}).attr('r', 0).attr('cy', function (d) {
+						return yScale(d.rank);
+					});
+
+					winsSelection.transition().delay(dir === 0 ? 0 : EXIT_DURATION).duration(SECOND * 2).ease('elastic').attr('r', function (d) {
+						return d.bottom || d.top ? radiusLarge : radiusSmall;
+					}).attr('cx', function (d) {
+						return xScale(d.seasonFormatted);
+					}).attr('cy', function (d) {
+						return yScale(d.rank);
+					});
+
+					var count = stepData.stretches.length;
+					document.querySelector('.madlib-count').innerHTML = count ? 'have made their journey from the bottom to the top <strong class=\'top\'>' + COUNT_TO_WORD[count] + '</strong> time' + (count === 1 ? '' : 's') + ' in franchise history.' : 'have never completed their quest to finish in the top four after starting from the bottom.';
+
+					var recent = count ? stepData.stretches[count - 1].length - 1 : 0;
+					document.querySelector('.madlib-detail').innerHTML = count ? 'Their most recent ascent was ' + getAverageDiff(recent) + ' average, spanning <strong class=\'bottom\'>' + recent + '</strong> seasons.' : 'Maybe next year will be their year...';
+					break;
+				}
+
+			case 'stretch-all':
 				{
 					break;
 				}
 
-			case 'path-normalized':
+			case 'stretch-normalized':
 				{
 					break;
 				}
 
-			case 'path-duration':
+			case 'stretch-duration':
 				{
 					break;
 				}
@@ -182,9 +308,16 @@
 		}
 
 		// EXIT
-		lineSelection.exit().remove();
+		allSelection.exit().transition().duration(EXIT_DURATION).style('opacity', 0).remove();
 
-		dotSelection.exit().transition().duration(EXIT_DURATION).style('opacity', 0).remove();
+		stretchSelection.exit().transition().duration(EXIT_DURATION).style('opacity', 0).remove();
+
+		winsSelection.exit().transition().duration(EXIT_DURATION).style('opacity', 0).remove();
+	}
+
+	function updateSingleStep() {
+		singleTeam = this.value;
+		if (previousStep === 2) stepGraphic(2);
 	}
 
 	function setupGraphScroll() {
@@ -194,9 +327,28 @@
 	function handleDataLoaded(err, result) {
 		data = cleanData(result);
 
-		dataByTeam = d3.nest().key(function (d) {
+		var byTeam = d3.nest().key(function (d) {
 			return d.name;
 		}).entries(data);
+
+		dataByTeam = byTeam.map(function (d) {
+			d.values = addStretches(d.values);
+			return d;
+		}).map(function (d) {
+			var _calculateStretch = calculateStretch(d);
+
+			var indices = _calculateStretch.indices;
+			var completed = _calculateStretch.completed;
+
+			d.stretches = { indices: indices, completed: completed };
+			return d;
+		});
+
+		var comp = dataByTeam.reduce(function (previous, current) {
+			return previous.concat(current.stretches.completed);
+		}, []);
+		stretchesMedian = d3.median(comp);
+		stretchesCompleted = comp.length;
 
 		// setup chart
 		chartWidth = outerWidth - MARGIN.left - MARGIN.right;
@@ -226,15 +378,34 @@
 
 		chartGroup.append('g').attr('class', 'axis axis--y').attr('transform', translate(0, 0)).call(yAxis);
 
-		chartGroup.append('g').attr('class', 'line-group');
+		chartGroup.append('g').attr('class', 'all-group');
 
-		chartGroup.append('g').attr('class', 'dot-group');
+		chartGroup.append('g').attr('class', 'stretch-group');
 
-		// createDropdown()
+		chartGroup.append('g').attr('class', 'wins-group');
+
 		setupGraphScroll();
 	}
 
+	function createDropdown() {
+		var el = document.querySelector('.madlib-name');
+		var html = Object.keys(TEAM_NAME_DICT).map(function (key) {
+			var selected = key === 'GSW' ? ' selected' : '';
+			return '<option' + selected + ' value=\'' + key + '\'>' + TEAM_NAME_DICT[key] + '</option>';
+		}).join('\n');
+		el.innerHTML = html;
+
+		el.addEventListener('change', updateSingleStep);
+	}
+
 	function init() {
+		var w = document.getElementById('container').offsetWidth;
+		outerWidth = w - SECTION_WIDTH - GRAPHIC_MARGIN;
+		outerHeight = Math.round(window.innerHeight - GRAPHIC_MARGIN * 2);
+		radiusSmall = Math.max(4, Math.round(outerHeight / 200));
+		radiusLarge = Math.round(radiusSmall * 1.5);
+
+		createDropdown();
 		d3.json('data/output.json', handleDataLoaded);
 	}
 
--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 (function() {
-	const STEPS = ['top-and-bottom', 'path-single', 'path-all', 'path-normalized', 'path-duration']
-	const RADIUS_LARGE = 6
-	const RADIUS_SMALL = 4
+	const TEAM_NAME_DICT = { 'ATL': 'Hawks','BOS': 'Celtics','BRK': 'Nets','CHI': 'Bulls','CHO': 'Hornets','CLE': 'Cavaliers','DAL': 'Mavericks','DEN': 'Nuggest','DET': 'Pistons','GSW': 'Warriors','HOU': 'Rockets','IND': 'Pacers','LAC': 'Clippers','LAL': 'Lakers','MEM': 'Grizzlies','MIA': 'Heat','MIL': 'Bucks','MIN': 'Timberwolves','NOP': 'Pelicans','NYK': 'Knicks','OKC': 'Thunder','ORL': 'Magic','PHI': '76ers','PHO': 'Suns','POR': 'Trail Blazers','SAC': 'Kings','SAS': 'Spurs','TOR': 'Raports','UTA': 'Jazz', WAS: 'Wizards' }
+	const COUNT_TO_WORD = ['zero', 'one', 'two', 'three', 'four', 'five']
+	const STEPS = ['top-and-bottom', 'warriors', 'stretch-single', 'stretch-all', 'stretch-normalized', 'stretch-duration']
 	const SECOND = 1000
 	const EXIT_DURATION = SECOND
 	const MARGIN = { top: 20, right: 40, bottom: 40, left: 40 }
-	const outerWidth = 640
-	const outerHeight = 480
-
+	const GRAPHIC_MARGIN = 20
+	const RATIO = 16 / 9
+	const SECTION_WIDTH = 320
+	
+	let singleTeam = 'GSW'
+	let outerWidth = 0
+	let outerHeight = 0
+	let radiusSmall = 0
+	let radiusLarge = 0
 	let previousStep = 0
 	let dir = 0
 	let chartWidth = 0
@@ -15,10 +21,18 @@
 	let data = []
 	let dataByTeam = []
 	let svg = null
+	let stretchesCompleted = 0
+	let stretchesMedian = 0
 	
 	const INTERPOLATE = 'step'
 	const xScale = d3.time.scale()
 	const yScale = d3.scale.linear()
+	// const createLineAll = d3.svg.line()
+	// 	.defined(d => d.rank)
+	// 	.interpolate(INTERPOLATE)
+	// 	.x(d => xScale(d.seasonFormatted))
+	// 	.y(d => yScale(d.rank))
+
 	const createLine = d3.svg.line()
 		.defined(d => d.rank)
 		.interpolate(INTERPOLATE)
@@ -35,47 +49,107 @@
 			d.seasonFormatted = yearFormat.parse(d.seasonYear)
 			d.id = index
 			return d
-		})	
+		})
 	}
 
-	function createDropdown() {
-		const options = dataByTeam.map(team =>
-			`<option>${team.key}</option>`
-		).join()
-		const el = document.querySelector('.teams')
-		el.innerHTML = options
-		el.addEventListener('change', e => {
-			const text = e.target.options[e.target.selectedIndex].text
-			drawTeam(text)
+	function calculateStretch(team) {
+		const indices = team.values.map((v, i) =>
+				v.start || v.stop ? i : -1
+			)
+			.filter(v => v > -1)
+		
+		const len = indices.length
+		const newLen = Math.floor(len / 2) * 2
+		const sliced = indices.slice(0, newLen)
+		const completed =  sliced
+			.map((v, i) => {
+				const streak = i % 2 ? v - sliced[i - 1] : -1
+				return streak
+			})
+			.filter(v => v > 0)
+			.reduce((previous, current) => previous.concat(current), [])
+
+		return { indices, completed }
+	}
+
+	function addStretches(values) {
+		let active = false
+		return values.map(season => {
+			const { bottom, top } = season
+			if (bottom && !active) {
+				season.start = true
+				season.stretch = true
+				active = true
+			} else if (top && active) {
+				season.stop = true
+				season.stretch = true
+				active = false
+			}
+			if (active) season.stretch = true
+			return season
 		})
+	}
+
+	function getStretches(team) {
+		const indices = team.stretches.indices
+
+		const len = indices.length
+		const newLen = Math.floor(len / 2) * 2
+		const sliced = indices.slice(0, newLen)
+
+		const stretches = sliced.map((index, i) => {
+			const a = index + 1
+			const b = sliced[i - 1]
+			return i % 2 ? team.values.slice(b, a) : null
+		}).filter(d => d)
+		
+		return stretches
+	}
+
+	function getAverageDiff(count) {
+		const diff = count - stretchesMedian
+		if (diff < 2) {
+			return 'shorter than'
+		} else if (diff > 2) {
+			return 'longer than'
+		} else {
+			return 'about'
+		}
 	}
 
 	function getStepData(step) {	
 		switch(step) {
 		case 'top-and-bottom': {
 			return {
-				line: [],
-				dot: data.filter(d => d.wins),
+				all: [],
+				wins: data.filter(d => d.wins),
+				stretches: [],
 			}
 		}
 			
-		case 'path-single': {
+		case 'warriors': {
 			const team = dataByTeam.filter(d => d.key === 'GSW')
 			return {
-				line: team,
-				dot: team[0].values.filter(d => d.wins),
+				all: team,
+				wins: team[0].values.filter(d => d.wins),
+				stretches: [],
 			}
 		}
 			
-		case 'path-all': {
+		case 'stretch-single': {
+			const team = dataByTeam.filter(d => d.key === singleTeam)
+			return {
+				all: team,
+				wins: team[0].values.filter(d => d.wins),
+				stretches: getStretches(team[0]),
+			}
+		}
+			
+		case 'stretch-normalized': {
 
 		}
 			
-		case 'path-normalized': {
-
-		}
-			
-		case 'path-duration': {
+		case 'stretch-duration': {
 
 		}
 			
@@ -91,7 +165,8 @@
 
 	function transitionPath(path) {
 		path.transition()
-			.duration(SECOND * 4)
+			.ease('quad-in-out')
+			.duration(SECOND * 3)
 			.attrTween('stroke-dasharray', tweenDash)
 	}
 
@@ -100,67 +175,113 @@
 		previousStep = step
 
 		const chartGroup = svg.select('.chart')
-		const lineGroup = chartGroup.select('.line-group')
-		const dotGroup = chartGroup.select('.dot-group')
+		const allGroup = chartGroup.select('.all-group')
+		const winsGroup = chartGroup.select('.wins-group')
+		const stretchGroup = chartGroup.select('.stretch-group')
 
 		// DATA
 		const stepData = getStepData(STEPS[step])		
-		const lineSelection = lineGroup.selectAll('.line').data(stepData.line)
-		const dotSelection = dotGroup.selectAll('.dot').data(stepData.dot, d => d.id)
+		const allSelection = allGroup.selectAll('.all').data(stepData.all)
+		const winsSelection = winsGroup.selectAll('.wins').data(stepData.wins, d => d.id)
+		const stretchSelection = stretchGroup.selectAll('.stretch').data(stepData.stretches)
+
+		console.log(stepData)
 
 		// UPDATE
 		switch(STEPS[step]) {
 		case 'top-and-bottom': {
-			dotSelection.enter()
+			winsSelection.enter()
 				.append('circle')
-					.attr('class', d => `dot ${d.bottom ? 'bottom' : ''} ${d.top ? 'top' : ''}`)
+					.attr('class', d => `wins ${d.bottom ? 'bottom' : ''} ${d.top ? 'top' : ''}`)
 					.attr('r', 0)
 					.attr('cx', d => xScale(d.seasonFormatted))
 					.attr('cy', d => yScale(d.rank))
 
-			dotSelection
+			winsSelection
 				.transition()
 				.duration(SECOND * 2)
-				.delay(d => d.rank * 75)
+				.delay(d => d.rank * 75 + (dir === 0 ? 0 : EXIT_DURATION))
 				.ease('quad-in-out')
-				.attr('r', RADIUS_SMALL)
+				.attr('r', radiusSmall)
 			break
 		}
 			
-		case 'path-single': {
-			lineSelection.enter()
+		case 'warriors': {
+			allSelection.enter()
 				.append('path')
-					.attr('class', 'line')
+					.attr('class', 'all')
 
-			lineSelection.attr('d', d => createLine(d.values))
+			allSelection.attr('d', d => createLine(d.values))
 				.call(transitionPath)
 
-			dotSelection.enter()
+			winsSelection.enter()
 				.append('circle')
-					.attr('class', d => `dot ${d.bottom ? 'bottom' : ''} ${d.top ? 'top' : ''}`)
+					.attr('class', d => `wins ${d.bottom ? 'bottom' : ''} ${d.top ? 'top' : ''}`)
 					.attr('r', 0)
 					.attr('cy', d => yScale(d.rank))
 
-			dotSelection
+			winsSelection
 				.transition()
 				.delay(EXIT_DURATION)
 				.duration(SECOND * 2)
 				.ease('elastic')
-				.attr('r', d => d.bottom || d.top ? RADIUS_LARGE : RADIUS_SMALL)
+				.attr('r', d => d.bottom || d.top ? radiusLarge : radiusSmall)
 				.attr('cx', d => xScale(d.seasonFormatted))
 				.attr('cy', d => yScale(d.rank))
 			break
 		}
-			
-		case 'path-all': {
+		
+		case 'stretch-single': {
+			allSelection.enter()
+				.append('path')
+					.attr('class', 'all')
+
+			allSelection
+				.attr('d', d => createLine(d.values))
+
+			stretchSelection.enter()
+				.append('path')
+					.attr('class', 'stretch')
+
+			stretchSelection.attr('d', d => createLine(d))
+				.call(transitionPath)
+
+			winsSelection.enter()
+				.append('circle')
+					.attr('class', d => `wins ${d.bottom ? 'bottom' : ''} ${d.top ? 'top' : ''}`)
+					.attr('r', 0)
+					.attr('cy', d => yScale(d.rank))
+
+			winsSelection
+				.transition()
+				.delay(dir === 0 ? 0 : EXIT_DURATION)
+				.duration(SECOND * 2)
+				.ease('elastic')
+				.attr('r', d => d.bottom || d.top ? radiusLarge : radiusSmall)
+				.attr('cx', d => xScale(d.seasonFormatted))
+				.attr('cy', d => yScale(d.rank))
+
+			const count = stepData.stretches.length
+			document.querySelector('.madlib-count').innerHTML = count
+				? `have made their journey from the bottom to the top <strong class='top'>${COUNT_TO_WORD[count]}</strong> time${count === 1 ? '' : 's'} in franchise history.`
+				: 'have never completed their quest to finish in the top four after starting from the bottom.'
+
+			const recent = count ? stepData.stretches[count - 1].length  - 1 : 0
+			document.querySelector('.madlib-detail').innerHTML = count
+				? `Their most recent ascent was ${getAverageDiff(recent)} average, spanning <strong class='bottom'>${recent}</strong> seasons.`
+				: 'Maybe next year will be their year...'
+			break
+		}
+
+		case 'stretch-all': {
 			break
 		}
 			
-		case 'path-normalized': {
+		case 'stretch-normalized': {
 			break
 		}
 			
-		case 'path-duration': {
+		case 'stretch-duration': {
 			break
 		}
 			
@@ -168,15 +289,29 @@
 		}
 
 		// EXIT
-		lineSelection.exit().remove()
+		allSelection.exit()
+			.transition()
+			.duration(EXIT_DURATION)
+			.style('opacity', 0)
+			.remove()
 
-		dotSelection
-			.exit()
+		stretchSelection.exit()
+			.transition()
+			.duration(EXIT_DURATION)
+			.style('opacity', 0)
+			.remove()
+
+		winsSelection.exit()
 			.transition()
 			.duration(EXIT_DURATION)
 			.style('opacity', 0)
 			.remove()
 		
+	}
+
+	function updateSingleStep() {
+		singleTeam = this.value
+		if (previousStep === 2) stepGraphic(2)
 	}
 
 	function setupGraphScroll() {
@@ -190,9 +325,24 @@
 	function handleDataLoaded(err, result) {
 		data = cleanData(result)
 		
-		dataByTeam = d3.nest()
+		const byTeam = d3.nest()
 			.key(d => d.name)
 			.entries(data)
+
+		dataByTeam = byTeam
+			.map(d => {
+				d.values = addStretches(d.values)
+				return d
+			})
+			.map(d => {
+				const { indices, completed } = calculateStretch(d)
+				d.stretches = { indices, completed } 
+				return d
+			})
+
+		const comp = dataByTeam.reduce((previous, current) => previous.concat(current.stretches.completed), [])
+		stretchesMedian = d3.median(comp)
+		stretchesCompleted = comp.length
 
 		// setup chart
 		chartWidth = outerWidth - MARGIN.left - MARGIN.right
@@ -238,16 +388,36 @@
 			.call(yAxis)
 
 		chartGroup.append('g')
-			.attr('class', 'line-group')
+			.attr('class', 'all-group')
 
 		chartGroup.append('g')
-			.attr('class', 'dot-group')
+			.attr('class', 'stretch-group')
 
-		// createDropdown()
+		chartGroup.append('g')
+			.attr('class', 'wins-group')
+
 		setupGraphScroll()
 	}
 
+	function createDropdown() {
+		const el = document.querySelector('.madlib-name')
+		const html = Object.keys(TEAM_NAME_DICT).map(key => {
+			const selected = key === 'GSW' ? ' selected' : ''
+			return `<option${selected} value='${key}'>${TEAM_NAME_DICT[key]}</option>`
+		}).join('\n')
+		el.innerHTML = html
+
+		el.addEventListener('change', updateSingleStep)
+	}
+
 	function init() {
+		const w = document.getElementById('container').offsetWidth
+		outerWidth = w - SECTION_WIDTH - GRAPHIC_MARGIN
+		outerHeight = Math.round(window.innerHeight - GRAPHIC_MARGIN * 2)
+		radiusSmall = Math.max(4, Math.round(outerHeight / 200))
+		radiusLarge = Math.round(radiusSmall * 1.5)
+
+		createDropdown()
 		d3.json('data/output.json', handleDataLoaded)
 	}
 
